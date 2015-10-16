e0fc17034730e22690ceb42ac009583b18aaf832
Andreas Gillström <andreasgillstrom@gmail.com>
2015-10-16 18:53:50 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-'use strict';
-var test = require('ava');
-var arrayOccurrence = require('./');
+import test from 'ava';
+import arrayOccurrence from './';
 
-test(function (t) {
+test(t => {
 	t.is(arrayOccurrence([], 1), 0);
 	t.is(arrayOccurrence([1, 2, 3, 4, 5, 3], 3), 2);
 	t.is(arrayOccurrence(['a', 'a', 'b', 'c', 'a', 'b'], 'a'), 3);
